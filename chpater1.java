0ae8c42cfc12f28b9624f43c7d7baa18b2af9cbf
jinoSon <solod85@gmail.com>
2015-01-28 18:19:01 +0900

    No.8 Replace Temp with Query

--- chpater1.java
+++ chpater1.java
@@ -73,10 +73,7 @@
  		Enumeration rentals = _rentals.elements();
  		String result = "Rental Record for " + getName() + "\n";
  		while (rentals.hasMoreElements()) {
- 			double thisAmount = 0;
  			Rental each = (Rental) rentals.nextElement();
-
- 			thisAmount = each.getCharge();
 
  // add frequent renter points
  			frequentRenterPoints ++;
@@ -86,8 +83,8 @@
  				each.getDaysRented() > 1) frequentRenterPoints ++;
  //show figures for this rental
  				result += "\t" + each.getMovie().getTitle()+ "\t" +
- 			String.valueOf(thisAmount) + "\n";
- 			totalAmount += thisAmount;
+ 			String.valueOf( each.getCharge()) + "\n";
+ 			totalAmount +=  each.getCharge();
  		}
  //add footer lines
  		result += "Amount owed is " + String.valueOf(totalAmount) +
